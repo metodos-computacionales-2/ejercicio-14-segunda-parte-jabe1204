ab56e2da1790368bd7bbf9549d0f4fa60408833b
jabe1204 <tjabet12@gmail.com>
2019-11-19 02:00:29 +0000

    Mejoro el código para gráficas cada método por aparte

--- graficador.py
+++ graficador.py
@@ -7,14 +7,14 @@ eulery = euler[:,1]
 eulervy = euler[:,2]
 
 plt.figure(1)
-plt.figure(figsize=(12,6))
+plt.figure(figsize=(10,4))
 ##Para el metodo de euler.
 plt.subplot(1,2,1)
 plt.plot(eulert,eulery)
 plt.grid()
 plt.xlabel("Tiempo(s)")
 plt.ylabel("Posicion(m)")
-plt.title("Oscilador armonico, posicion vs tiempo")
+plt.title("Oscilador armonico, Posicion vs Tiempo")
 
 plt.subplot(1,2,2)
 plt.plot(eulery,eulervy)
@@ -22,7 +22,7 @@ plt.grid()
 plt.xlabel("Posicion(m)")
 plt.ylabel("Velocidad(m/s)")
 plt.title("Oscilador armonico, velocidad vs tiempo")
-
+plt.savefig("OsciladorEuler.png")
 
 data = np.loadtxt("datos.dat")
 tiempo = data[:,0]
@@ -30,25 +30,41 @@ y = data[:,1]
 vy = data[:,2]
 ## Para el método de Rk4.
 plt.figure(2)
+plt.figure(figsize=(10,4))
 plt.subplot(1,2,1)
 plt.plot(tiempo,y)
 plt.grid()
 plt.xlabel("Tiempo(s)")
 plt.ylabel("Posicion(m)")
+plt.title("Oscilador armonico, Posicion vs Tiempo")
 
 plt.subplot(1,2,2)
 plt.plot(y,vy)
 plt.grid()
+plt.xlabel("Posicion(m)")
+plt.ylabel("Velocidad(m/s)")
+plt.title("Oscilador armonico, Velocidad vs Tiempo")
+
+plt.savefig("OsciladorRk4.png")
 
 ## Para el Rk4 con friccion.
 yfriccion = data[:,3]
 vyfriccion = data[:,4]
 plt.figure(3)
+plt.figure(figsize=(10,4))
+
 plt.subplot(1,2,1)
 plt.plot(tiempo,yfriccion)
+plt.xlabel("Tiempo(s)")
+plt.ylabel("Posicion(m)")
 plt.grid()
+plt.title("Oscilador armonico, Posicion vs Tiempo")
+
 plt.subplot(1,2,2)
 plt.plot(yfriccion,vyfriccion)
 plt.grid()
+plt.xlabel("Posicion(m)")
+plt.ylabel("Velocidad(m/s)")
+plt.title("Oscilador armonico, Velocidad vs Tiempo")
 
-plt.savefig("rk4.png")
+plt.savefig("OsciladorRk4Friccion.png")

03b0561ddfd418dae7faa56960bb40757e308f4d
jabe1204 <tjabet12@gmail.com>
2019-11-19 00:59:26 +0000

    Se realizaron los tres metodos en un .cpp

---
+++ lab14_Parte2.cpp
@@ -0,0 +1,109 @@
+#include <cmath>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+const double k = 100;
+const double m = 2;
+const double lambda = 1;
+const double G = 0.7;
+const double dt = 0.01;
+
+double velocidady(double t,double y, double vy); 
+double aceleraciony(double t,double y, double vy);
+void euler(double y, double vy, double tf, string name);
+void rk4(double t, double dt, double &y, double &vy);
+void rk4confriccion(double t, double dt, double &y, double &vy);
+
+int main()
+{
+	double y,vy,tf,yf,vyf;
+    tf = 10.0;
+	y = 1.0;
+	vy = 0.0;
+    yf = 1.0;
+    vyf = 0.0;
+    
+    euler(y,vy,tf,"euler.dat");
+    
+    ofstream outfile;
+    outfile.open("datos.dat");
+	for(double time = 0.0; time <= tf; time += dt)
+    {
+		outfile <<time<< " " << y << " " << vy << " " << yf << " " << vyf<<endl;
+		rk4(time, dt, y, vy);
+        rk4confriccion(time, dt,yf,vyf);
+    }
+    outfile.close();
+    return 0;
+}
+
+double velocidady(double t, double y, double vy)
+{
+  return vy;
+}
+
+double aceleraciony(double t, double y, double vy)
+{
+  return (-k/m)*pow(y,lambda);
+}
+
+double aceleracionyfriccion(double t, double y, double vy)
+{
+  return (-k/m)*pow(y,lambda) - G*vy;
+}
+
+void euler(double y, double vy, double tf, string name)
+{
+    double ya,va,ti;
+    
+    ofstream outfile;
+    outfile.open(name);
+    
+    for(double ti=0; ti<= tf; ti+=dt)
+    {
+        va = vy;
+        ya = y;
+        y += dt*va;
+        vy -= dt*(k/m)*ya;
+        outfile<< ti <<" "<<y<< " "<<vy<<endl;
+    }
+    outfile.close();
+}
+
+void rk4(double t, double dt, double &y, double &vy) 
+{
+    double k1y, k1vy, k2y, k2vy, k3y, k3vy, k4y, k4vy;
+    
+    k1y = dt*velocidady(t, y, vy);
+    k2y = dt*velocidady(t+dt/2, y + k1y/2, vy + k1vy/2);
+    k3y = dt*velocidady(t+dt/2, y + k2y/2, vy + k2vy/2);
+    k4y = dt*velocidady(t + dt, y + k3y, vy + k3vy);
+    
+    k1vy = dt*aceleraciony(t, y, vy);
+    k2vy = dt*aceleraciony(t+dt/2, y + k1y/2, vy + k1vy/2);
+    k3vy = dt*aceleraciony(t+dt/2, y + k2y/2, vy + k2vy/2);
+    k4vy = dt*aceleraciony(t + dt, y + k3y, vy + k3vy);
+    
+	y = y + (1.0/6.0)*(k1y + 2*k2y + 2*k3y + k4y);
+    vy = vy + (1.0/6.0)*(k1vy + 2*k2vy + 2*k3vy + k4vy);
+}
+
+
+void rk4confriccion(double t, double dt, double &y, double &vy) 
+{
+    double k1y, k1vy, k2y, k2vy, k3y, k3vy, k4y, k4vy;
+    
+    k1y = dt*velocidady(t, y, vy);
+    k2y = dt*velocidady(t+dt/2, y + k1y/2, vy + k1vy/2);
+    k3y = dt*velocidady(t+dt/2, y + k2y/2, vy + k2vy/2);
+    k4y = dt*velocidady(t + dt, y + k3y, vy + k3vy);
+    
+    k1vy = dt*aceleracionyfriccion(t, y, vy);
+    k2vy = dt*aceleracionyfriccion(t+dt/2, y + k1y/2, vy + k1vy/2);
+    k3vy = dt*aceleracionyfriccion(t+dt/2, y + k2y/2, vy + k2vy/2);
+    k4vy = dt*aceleracionyfriccion(t + dt, y + k3y, vy + k3vy);
+    
+	y = y + (1.0/6.0)*(k1y + 2*k2y + 2*k3y + k4y);
+    vy = vy + (1.0/6.0)*(k1vy + 2*k2vy + 2*k3vy + k4vy);
+}
